fae872d92de9889de4f6af2b827ad08795923fb2
SaraPOliveira <150864914+SaraPOliveira@users.noreply.github.com>
2025-11-17 22:19:29 -0300

    Add files via upload

---
+++ comandos/comandos.sh
@@ -0,0 +1,86 @@
+ 1  curl -fsS https://dl.brave.com/install.sh | sh~
+    2  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg~
+    3  sudo apt install apt-transport-https curl software-properties-common
+    4  curl -fsSL https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/brave-browser-archive-keyring.gpg
+    5  sudo apt install apt-transport-https curl software-properties-common
+    6  curl -fsSL https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/brave-browser-archive-keyring.gpg
+    7  brave-browser
+    8  curl -fsS https://dl.brave.com/install.sh | sh
+    9  sudo apt install chromium-browser
+   10  sudo dnf install chromium
+   11  npx @puppeteer/browsers install chrome@stable
+   12  @puppeteer/browsers install chrome@stable
+   13  sudo apt update
+   14  sudo apt install chromium-browser
+   15  apt install chromium-browser
+   16  sudo apt update
+   17  sudo apt install chromium-browser
+   18  sudo apt update
+   19  sudo apt install chromium-browser
+   20  sudo apt update
+   21  sudo apt install snapd
+   22  sudo systemctl enable --now snapd.socket
+   23  sudo apt update
+   24  sudo apt install torbrowser-launcher
+   25  sudo apt install torbrowser-launcher
+   26  sudo apt update
+   27  sudo apt install git vim notepadqq 
+   28  vim
+   29  sudo init 0
+   30  sudo apt update
+   31  sudo -i
+   32  sudo apt upgrade
+   33  sudo apt upgrade -y
+   34  sudo apt upgrade
+   35  sudo apt dist-upgrade 
+   36  sudo apt full-upgrade 
+   37  sudo apt autoremove
+   38  sudo apt autoclean
+   39  sudo apt clean
+   40  sudo apt update
+   41  sudo apt install git
+   42  sudo apt  install git vim pip python
+   43  sudo apt install git vim pip python3 pip
+   44  git clone https://github.com/SaraPOliveira/linuxmint
+   45  git config --global user.name "Sara Oliveira"
+   46  git config --global user.email saoliveira425@gmail.com
+   47  git config --global core.editor vim
+   48  git config --global merge.tool vimdiff
+   49  git config --global color.ui true
+   50  git config --list
+   51  git clone
+   52  got clone https://github.com/SaraPOliveira/linuxmint
+   53  git clone https://github.com/SaraPOliveira/linuxmint
+   54  git config --global user.name "Sara Oliveira"
+   55  git config --global user.email saoliveira425@gmail.com
+   56  git config --global core.editor vim
+   57  git config --global marge.tool vimdiff
+   58  git config --global color.ui true
+   59  git config --list
+   60  git clone https://github.com/SaraPOliveira/linuxmint
+   61  git pull
+   62  git add .
+   63  git commit -m "Atualização"
+   64  git pull
+   65  git add .
+   66  git commit -"Atualização"
+   67  git commit -m "Atualização"
+   68  git push
+   69  git pull
+   70  git add .
+   71  git commit -m "Atualizado"
+   72  git push
+   73  git clone https://github.com/SaraPOliveira/linuxmint
+   74  git push
+   75  git pull
+   76  git add .
+   77  git commit -m "Atualização"
+   78  git push
+   79  git clone https://github.com/SaraPOliveira/linuxmint
+   80  git pull
+   81  git add .
+   82  git commit -m "Up"
+   83  git push
+   84  git clone https://github.com/SaraPOliveira/linuxmint
+   85  hidtory
+   86  history
